ce99f2084192cd4a9e0f68a152524e1d998859f0
Lina Sekfali <lina.sekfali@ensia.edu.dz>
2026-01-10 21:56:54 +0100

    testtt

--- rise/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rise/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,10 @@ import app_links
 import audioplayers_darwin
 import file_selector_macos
 import firebase_core
+import firebase_crashlytics
 import firebase_messaging
 import flutter_local_notifications
+import gal
 import path_provider_foundation
 import record_macos
 import rive_common
@@ -23,8 +25,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
